a5974aa34abb84b6180a37385edd174ec5981bf1
Svenexy <33456257+Svenexy@users.noreply.github.com>
2020-12-17 22:59:34 +0100

    Add project files.

---
+++ SerialInterfaceClock/ComboBoxSerial.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SerialInterfaceClock
+{
+    public partial class ComboBoxSerial
+    {
+
+        /// <summary>
+        /// public class SerialPortItem
+        /// </summary>
+        public class SerialportItem
+        {
+            public string Name;
+            public string Value;
+
+            /// <summary>
+            /// Set Item name and value
+            /// </summary>
+            /// <param name="name"></param>
+            /// <param name="value"></param>
+            public SerialportItem(string name, string value)
+            {
+                Name = name; Value = value;
+            }
+
+            /// <summary>
+            /// Returns Item name
+            /// </summary>
+            /// <returns></returns>
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+
+        public void GetPortsAddToComboBox(ComboBox CbPorts)
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort"))
+            {
+                List<SerialportItem> ListSerialPorts = new List<SerialportItem>();
+
+                /* Get portnames and add them to the string array */
+                string[] PortNames = SerialPort.GetPortNames();
+
+                /* Get serialports from system, get portname and device name and add them to tList */
+                var Ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                var tList = (from n in PortNames join p in Ports on n equals p["DeviceID"].ToString() select n).ToList();
+
+                if (PortNames != null || PortNames.Length != 0)
+                {
+                    List<string> portsOrdered = new List<string>();
+
+                    /* Sort all items in list on alphabetical order */
+                    Array.Sort<string>(PortNames);          // Call sort.
+
+                    foreach (var c in PortNames)
+                    {
+                        portsOrdered.Add(c);
+                    }
+
+                    tList.Sort();
+
+                    /* Run thru list and add only the items that contains a device name to ListSerialPorts */
+                    for (int x = 0; x < portsOrdered.Count(); x++)
+                    {
+                        for (int i = 0; i < tList.Count; i++)
+                        {
+                            if (tList[i].Contains(portsOrdered[x]))
+                            {
+                                ListSerialPorts.Add(new SerialportItem(tList[i], portsOrdered[x]));
+                                portsOrdered.RemoveAt(x);
+                            }
+                        }
+                    }
+
+                    /* Check if the list count is higher than zero */
+                    if (portsOrdered.Count() > 0)
+                    {
+                        /* Run thru the list and add the port with new name to ListSerialPorts */
+                        for (int x = 0; x < portsOrdered.Count(); x++)
+                        {
+                            ListSerialPorts.Add(new SerialportItem((portsOrdered[x] + " - USB Serial Port"), portsOrdered[x]));
+                        }
+                    }
+                }
+
+                /* Clear combobox */
+                CbPorts.Items.Clear();
+                CbPorts.Text = string.Empty;
+
+                /* Add serialports to combobox */
+                if (ListSerialPorts.Count > 0)
+                {
+                    for (int index = 0; index < ListSerialPorts.Count; index++)
+                        CbPorts.Items.Add(ListSerialPorts[index]);
+                }
+            }
+        }
+    }
+}
---
+++ SerialInterfaceClock/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace SerialInterfaceClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        ComboBoxSerial comboboxserial;
+        SerialPort serialPort;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            comboboxserial = new ComboBoxSerial();
+            serialPort = new SerialPort();
+            
+            comboboxserial.GetPortsAddToComboBox(CbSerialPort);
+            
+            
+            startclock();
+
+
+        }
+
+        private void Bt_Connect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                serialPort.PortName = CbSerialPort.Text;
+                serialPort.BaudRate = Convert.ToInt32(Cb_Baudrate.Text);
+                serialPort.DataBits = 8;
+                serialPort.Parity = Parity.None;
+                serialPort.Handshake = Handshake.None;
+                serialPort.StopBits = StopBits.One;
+                serialPort.DataReceived += SerialPortDataRecieved;
+                serialPort.Open();
+                Debug.WriteLine($"Connected to {CbSerialPort.Text} on baudrate {Cb_Baudrate.Text}");
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, error.Source  ,MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SerialPortDataRecieved(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            int dataLength = serialPort.BytesToRead;
+            byte[] dataRecieved = new byte[dataLength];
+            Debug.WriteLine(dataRecieved);
+            int nBytes = serialPort.Read(dataRecieved, 0, dataLength);
+            if (nBytes == 0) return;
+
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                Lb_Recieved.Items.Add($"{Encoding.Default.GetString(dataRecieved)} {Environment.NewLine}");
+            }));
+
+            
+            
+            //throw new NotImplementedException();
+        }
+
+
+        private void startclock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickevent;
+            timer.Start();
+
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            Lb_Time.Content = DateTime.Now.ToString("HH:mm:ss");
+            Lb_Date.Content = DateTime.Now.ToString("dd-MM-yyyy");
+        }
+    }
+}
